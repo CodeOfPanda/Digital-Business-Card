cee247faba3f1fc3dfbe83c2c5427fcbcdf3c6cb
CodeOfPanda <mleigla@wgu.edu>
2022-01-28 20:05:21 -0700

    added local profile picture

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
+import profilePicture from '../profilePicture.jpg'
+
 export default function Header() {
     return (
         <header>
-            <img src="https://www.maggieleigland.me/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fpublic%2Fstatic%2Fimages%2Fprofile.7e8a792ee6d8e48ce0430aadc0fd9930.png&w=256&q=75" alt="head shot of Maggie Leigland"/>
+            <img src={profilePicture} alt="head shot of Maggie Leigland"/>
             <h1 className="header--name">Maggie Leigland</h1>
             <h3 className="header--occupation">Computer Science Student</h3>
             <a className="header-a-link" href="https://maggieleigland.me"><h4 className="header--link">maggieleigland.me</h4></a>
         </header>
     )
 }
-
-// make my website a link 
